fcd1959f5d9c14acd9fb5262790f0252f2a3d1f5
VaibhavShindePurp <vaibhav.shinde@purpleradiance.com>
2025-01-10 17:21:46 +0530

    Added Axios Request For SIGNUP

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+        '/api': 'http://localhost:8080', // Adjust this to your backend URL
+    },
+},
   plugins: [react()],
 })
