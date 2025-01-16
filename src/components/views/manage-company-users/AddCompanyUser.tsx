7ae66551b82b6de362155bef88558f74a5d350be
VaibhavShindePurp <vaibhav.shinde@purpleradiance.com>
2025-01-16 18:01:15 +0530

    added hover effect to sidenavbar

--- src/components/views/manage-company-users/AddCompanyUser.tsx
+++ src/components/views/manage-company-users/AddCompanyUser.tsx
@@ -2,9 +2,9 @@ import AddCompanyUserForm from "../../forms/AddCompanyUserFrom";
 
 function AddCompanyUser() {
   return (
-    <main className="space-y-5 mt-32 flex items-center justify-center">
+    <div className="space-y-5 mt-32 flex items-center justify-center">
       <AddCompanyUserForm></AddCompanyUserForm>
-    </main>
+    </div>
   );
 }
 
--- src/components/views/manage-company-users/GetCompanyUsers.tsx
+++ src/components/views/manage-company-users/GetCompanyUsers.tsx
@@ -4,36 +4,33 @@ import { GetCompanyUsersList } from "../../lists/GetCompanyUsersList";
 import axios from "axios";
 import { useLoggedInUserContext } from "../../../context/user/LoggedInUserContext";
 
+function GetCompanyUsers() {
+  const [companyUsers, setCompanyUsers] = useState<companyUsersProps[]>([]);
+  const { loginStatus } = useLoggedInUserContext();
 
-function GetCompanyUsers(){
+  useEffect(() => {
+    axios.defaults.headers.common["Authorization"] =
+      "Bearer " + loginStatus.token;
+    const postData = {
+      company_id: loginStatus.companyId,
+      requestedby: loginStatus.userId,
+    };
+    axios
+      .post("/api/main/purple-crm-api/getcompanyuser", postData)
+      .then((response) => {
+        setCompanyUsers(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    const [companyUsers, setCompanyUsers] = useState<companyUsersProps[]>([]);
-    const{loginStatus} = useLoggedInUserContext();
-    
-    useEffect(() => {
-        axios.defaults.headers.common["Authorization"] = "Bearer "+ loginStatus.token;
-        const postData = {
-            company_id : loginStatus.companyId,
-            requestedby:loginStatus.userId
-        }
-        axios.post("/api/main/purple-crm-api/getcompanyuser",postData)
-        .then((response) => {
-            setCompanyUsers(response.data);
-            console.log(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-
-    }, []);
-    
-    
-
-    return (
-        <main>
-            <GetCompanyUsersList users={companyUsers}></GetCompanyUsersList>
-        </main>
-    )
+  return (
+    <div>
+      <GetCompanyUsersList users={companyUsers}></GetCompanyUsersList>
+    </div>
+  );
 }
 
 export default GetCompanyUsers;
