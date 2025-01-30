1d98fa735e16b84d6c20626becf17be20aff5753
VaibhavShindePurp <vaibhav.shinde@purpleradiance.com>
2025-01-30 15:18:44 +0530

    Edit Company User

---
+++ src/components/modals/EditCompanyUserModal.tsx
@@ -0,0 +1,135 @@
+
+import React, { useState } from 'react';
+import { EditIcon, X } from 'lucide-react';
+import FormInput from '../ui/FormInput';
+import Button from '../ui/Button';
+import { useLoggedInUserContext } from '../../context/user/LoggedInUserContext';
+import axios from 'axios';
+import MessageSnackBar from '../ui/MessageSnackbar';
+
+type EditUserPopupProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  user : {
+    company_id: number,
+    id : number,
+    fullname: string,
+    email: string,
+    mobilenumber: string,
+    createdby: string,
+    isactive: boolean,
+    requestedby: string,
+    generate_password: string,
+  }
+};
+
+export function EditCompanyUserModal({ isOpen, onClose,user }: EditUserPopupProps) {
+
+  const [updateUserformData,setUpdateUserFormData] = useState({
+    fullName: "",
+    mobilenumber : ""
+  })
+
+   const [snackbar, setSnackbar] = useState({
+      open: false,
+      message: '',
+      type: 'success' as 'success' | 'error',
+    });
+
+  const {loginStatus} = useLoggedInUserContext();
+  const handleEditUserFormChange = (event : React.ChangeEvent<HTMLInputElement>) => {
+      const {name,value} = event.target;
+      setUpdateUserFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  }
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const postUpdateUserData = {
+      id : user.id,
+      updatedby : loginStatus.userId,
+      company_id : loginStatus.companyId,
+      fullname : updateUserformData.fullName,
+      mobilenumber : updateUserformData.mobilenumber
+    }
+
+    axios.defaults.headers.common["Authorization"] =
+        "Bearer " + loginStatus.token;
+        axios.post("/api/main/purple-crm-api/update/company/users",postUpdateUserData)
+        .then(response => {
+          console.log(response.data);
+          showSnackbar(response.data.message,"success")
+
+        })
+        .catch(error => {
+          console.log(error);
+          showSnackbar(error.message,"error")
+        })
+
+  };
+  const showSnackbar = (message: string, type: 'success' | 'error') => {
+    setSnackbar({ open: true, message, type });
+  };
+
+  const handleCloseSnackbar = () => {
+    setSnackbar((prev) => ({ ...prev, open: false }));
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-10 flex items-center justify-center p-4">
+      <div className="bg-white rounded-lg shadow-xl w-full max-w-md relative animate-fadeIn">
+        <button
+          onClick={onClose}
+          className="absolute right-4 top-4 text-gray-400 hover:text-gray-600"
+        >
+          <X size={20} />
+        </button>
+
+        <div className="p-6">
+          <div className="flex items-center gap-3 mb-6">
+            <EditIcon className="text-blue-500" size={24} />
+            <h2 className="text-xl font-semibold text-gray-800">
+              Edit {user.fullname}
+            </h2>
+          </div>
+
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <FormInput
+              label="Name : "
+              type="text"
+              name="fullName"
+              placeholder="Enter User Name"
+              defaultValue={user.fullname}
+              maxLength={256}
+              onChange={handleEditUserFormChange}
+            />
+            <FormInput
+              label="Mobile Number : "
+              type="tel"
+              name="mobilenumber"
+              placeholder="Enter Mobile Number"
+              defaultValue={user.mobilenumber} 
+            />
+            <FormInput
+              label="Email : "
+              type="email"
+              name="email"
+              placeholder="Enter Email Address"
+              defaultValue={user.email}
+              readonly = {true}
+              
+            />
+            <Button type="submit">Update Company User</Button>
+          </form>
+        </div>
+      </div>
+       <MessageSnackBar
+        isOpen={snackbar.open}
+        message={snackbar.message}
+        type={snackbar.type}
+        onClose={handleCloseSnackbar}
+        duration={2000}
+      /> 
+    </div>
+  );
+}
--- src/components/ui/FormInput.tsx
+++ src/components/ui/FormInput.tsx
@@ -29,7 +29,9 @@ const FormInput = ({
   onBlur,
   error,
   center,
-  ref
+  ref,
+  defaultValue,
+  readonly
 }: FormInputProps) => {
   return (
     <div>
@@ -39,14 +41,20 @@ const FormInput = ({
       <div className="mt-1 relative">
         <input
         ref ={ref}
+        readOnly={readonly}
           type={type}
+          defaultValue={defaultValue}
           name={name}
           id={name}
           required={required}
           placeholder={placeholder}
           onChange={onChange}
           onBlur={onBlur}
-          className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+          className={readonly ? 
+            "appearance-none block w-full px-3 py-2 border bg-gray-300 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            :
+            "appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+           }
         />
         
         {rightElement && (
