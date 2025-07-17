0fc913b5d34faf8db2f6d141ce94d5af5de553a3
Hrutik Sargar <hrutik.sargar@purpleradiance.com>
2025-07-17 11:23:08 +0530

    Hrutik : code added for notification module

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
-
 // import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
 /**
  * @params {string} root: The id of the root element in the HTML file.
  * @function createRoot entry point for the react-dom library.
  */
-createRoot(document.getElementById('root')!).render(
-  
-
-    <App />
-
-  
-);
+createRoot(document.getElementById("root")!).render(<App />);
