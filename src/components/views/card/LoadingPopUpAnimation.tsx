d1fc77c67354015cc0b19e8c2b1c5d38abc0be47
Nitikesh Yewale <nitikesh.yewale@purpleradiance.com>
2026-01-09 11:39:26 +0530

    added wait corsor while loading spinner

--- src/components/views/card/LoadingPopUpAnimation.tsx
+++ src/components/views/card/LoadingPopUpAnimation.tsx
@@ -10,7 +10,7 @@ const LoadingPopUpAnimation : React.FC<LoadingPopupProps> = ({ show, text = "Loa
   if (!show) return null;
 
   return createPortal(
-    <div className="fixed inset-0 bg-black bg-opacity-5 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-5 flex items-center justify-center z-50 cursor-wait">
       <div className="bg-white rounded-2xl shadow-lg p-6  flex flex-col items-center space-y-4 animate-fadeIn">
         {/* Spinner */}
         <div className="w-8 h-8 border-4 border-blue-400 border-t-transparent rounded-full animate-spin" />
