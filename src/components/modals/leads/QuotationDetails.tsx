89b64e4ab711cdbfaa58186e8fe1991d5cfd56a1
Nitikesh Yewale <nitikesh.yewale@purpleradiance.com>
2026-04-14 17:42:56 +0530

    some bug fix in quotation template content block

--- src/components/modals/leads/QuotationDetails.tsx
+++ src/components/modals/leads/QuotationDetails.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import { useUserAccessModules } from "../../../config/hooks/useAccessModules";
 import Button from "../../ui/Button";
 import AccessDeniedMessagePage from "../../views/not-found/AccessDeniedMessagePage";
+import MESSAGE from "../../../constants/Messages";
 
 type QuotationDetailsProps = {
-  leadId: number;
+  id: number;
+  quotation_type_id: number;
 };
 
-const QuotationDetails: React.FC<QuotationDetailsProps> = ({ leadId }) => {
-  const { userHasAccessToViewQuotationTemplate, userHasAccessToAddQuotationTemplate } = useUserAccessModules();
-
-  if (!userHasAccessToViewQuotationTemplate) {
-    return <AccessDeniedMessagePage />;
-  }
+const QuotationDetails: React.FC<QuotationDetailsProps> = ({ id, quotation_type_id }) => {
+  const { userHasAccessToViewLeadQuotation, userHasAccessToAddLeadQuotation } = useUserAccessModules();
 
   return (
     <div className="w-full  shadow-lg ">
@@ -22,14 +20,12 @@ const QuotationDetails: React.FC<QuotationDetailsProps> = ({ leadId }) => {
             <span className="table-header-custom pl-1  text-center ">
               Quotation
             </span>
-            {userHasAccessToViewQuotationTemplate && userHasAccessToAddQuotationTemplate && (
+            {userHasAccessToAddLeadQuotation && (
               <div className="flex justify-end items-center text-xs gap-x-2  text-gray-500">
                 <Button
-                  disabled={!userHasAccessToAddQuotationTemplate}
+                  disabled={!userHasAccessToAddLeadQuotation}
                   className="bg-blue-600 hover:bg-blue-700 caption-custom white-text px-1 py-0.5 rounded-md flex items-center gap-1"
-                  onClick={() => {
-                   
-                  }}
+                  onClick={() => {}}
                 >
                   +Generate Quotation
                 </Button>
@@ -37,12 +33,20 @@ const QuotationDetails: React.FC<QuotationDetailsProps> = ({ leadId }) => {
             )}
           </div>
         </div>
-        <div className="bg-white overflow-y-auto rounded-lg shadow-sm p-2 h-[10vh]">
-          {`Lead Id: ${leadId}`}
-          Quotation List
-        </div>
+        {userHasAccessToViewLeadQuotation ? (
+          <div className="bg-white overflow-y-auto rounded-lg shadow-sm p-2 ">
+            {`Id: ${id}`}
+            <br />
+            {`Quotation Type Id: ${quotation_type_id}`}
+            <br />
+            Quotation List
+          </div>
+        ) : (
+          <AccessDeniedMessagePage
+            message={MESSAGE.MODULE_ACCESS.LEAD_QUOTATION.DENIED_VIEW_ACCESS}
+          />
+        )}
       </div>
-      
     </div>
   );
 };
