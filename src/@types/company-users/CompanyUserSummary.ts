96c68362f6c7edbaa7cef151bf9963a1ed21a785
PallaviChede2003 <pallavi.chede@purpleradiance.com>
2026-05-26 18:17:27 +0530

    Change in Company User and Task

---
+++ src/@types/company-users/CompanyUserSummary.ts
@@ -0,0 +1,8 @@
+type CompanyUserSummary = {
+  total_company_users: number;
+  total_active_users: number;
+  total_inactive_users: number;
+  total_users_created_this_month: number;
+};
+
+export default CompanyUserSummary;
---
+++ src/@types/my-task-management/GeneralTaskMasterSummary.ts
@@ -0,0 +1,6 @@
+export default interface GeneralTaskMasterSummary {
+  total_master_task_created: number;
+  total_master_task_high_priority: number;
+  total_master_task_recurring: number;
+  total_master_task_active: number;
+}
